bd41f0c8334a7dd99fded8a76e1cbcf3e98d54d0
Edward Kruljac <ewkruljac@gmail.com>
2016-08-14 00:07:52 -0600

    Still just takes input and displays game board, but (hopefully) this is smarter.

--- toh.rb
+++ toh.rb
@@ -1,42 +1,66 @@
 class TowersOfHanoi
 
 @tower_height = 0
-@tower_pieces = Array.new
-@player_move = Array.new
+#@tower_pieces = []
+@player_move = []
+@min_turns = 0
+@current_turn = 0
+@col_a = []
+@col_b = []
+@col_c = []
+@game_board = []
 
-  def init_game_board(tower_pieces)
-    @tower_pieces = tower_pieces
-
-    @tower_pieces.each_index do |ind|
-      (1+ind).times { print "o" }
-      puts ""
-    end
-
-    3.times do
-      @tower_pieces.length.times {print "-"}
-      print "\t"
-    end
-
-    puts ""
-  end
 
 
   def start_game
-    tower_pieces = []
+
     puts "Welcome to Towers of Hanoi!!\n"
     puts "Instructions:"
     puts "Enter where you'd like to move from and to"
     puts "in the format '1,3'.  Enter 'q' to quit."
     print "How tall will your tower be?\n>> "
-    @tower_height= gets.chomp
+    @tower_height = gets.chomp
 
-    for i in 0...@tower_height.to_i
-      tower_pieces.push(i)
-    end
-
-    return tower_pieces
+    return @tower_height.to_i
   end
 
+#----------
+
+  def init_col_a(tower_height)
+    col_a = []
+
+    for i in 1..tower_height do
+      col_a[i-1] = "o" * i
+    end
+
+    col_a.push("-" * tower_height)
+
+  end
+
+#---------
+
+  def init_empty_cols(tower_height)
+    cols = []
+
+    for i in 1..tower_height do
+      cols[i-1] = " " * i
+    end
+
+    cols.push("-" * tower_height)
+
+  end
+
+#----------
+
+  def display_game_board(col_a, col_b, col_c, tower_height)
+
+    for i in 0..tower_height do
+      puts "\t#{col_a[i]}\t#{col_b[i]}\t#{col_c[i]}"
+    end
+
+  end
+
+#-----------
 
   def get_player_move
 
@@ -45,11 +69,11 @@ class TowersOfHanoi
 
   end
 
-  def update_game_board(tower_pieces, player_move)
+#----------
 
-    col_a = []
-    col_b = []
-    col_c = []
+  def update_game_board(col_a, col_b, col_c, player_move)
+
+    
 
 
 
@@ -58,11 +82,15 @@ class TowersOfHanoi
 
   def game_loop
 
-    @tower_pieces = start_game
-    init_game_board(@tower_pieces)
+    @tower_height = start_game
+    @col_a = init_col_a(@tower_height)
+    @col_b = init_empty_cols(@tower_height)
+    @col_c = init_empty_cols(@tower_height)
+    display_game_board(@col_a, @col_b, @col_c, @tower_height)
     @player_move = get_player_move
     exit if @player_move[0] == "q"
-    update_game_board(@tower_pieces, @player_move)
+    @game_board = update_game_board(@col_a, @col_b, @col_c, @player_move)
+    #check_for_win if @current_turn >= @min_turns
 
   end
 
